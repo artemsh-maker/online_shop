db3b1b3e893b2726a5230456b9d4e108ccfb2dd5
artemsh-maker <84853424+artemsh-maker@users.noreply.github.com>
2024-06-16 19:25:32 +0300

    Add files via upload

--- src/utils.cpp
+++ src/utils.cpp
@@ -42,22 +42,22 @@ std::vector<char> loadImage(const std::string& filePath) {
 void init_logging_consol_and_file() {
     // Логирование в файл с ротацией по размеру и времени
     boost::log::add_file_log(
-        boost::log::keywords::file_name = "sample_%N.log",
+        boost::log::keywords::file_name = "../logs/sample_%N.log",
         boost::log::keywords::rotation_size = 10 * 1024 * 1024, // 10 MB
         boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0), // ежедневно в полночь
-        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%",
+        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%", // в таком виде: [2024-06-16 19:17:06.121083] [0x00007faee9e006c0] [info] Received request at /
         boost::log::keywords::auto_flush = true
     );
 
     // Логирование в консоль
     boost::log::add_console_log(
-        std::cout,
-        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+        std::cout, 
+        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%" // в таком виде: [2024-06-16 19:17:06.121083] [0x00007faee9e006c0] [info] Received request at /
     );  
 
     // Установка уровня логирования (опционально)
     boost::log::core::get()->set_filter(
-        boost::log::trivial::severity >= boost::log::trivial::info
+        boost::log::trivial::severity >= boost::log::trivial::debug
     );
 
     // Добавление общих атрибутов
