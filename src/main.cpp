fb9255621bdf4eb167d3b0120fc0be74ef3c2999
artemsh-maker <84853424+artemsh-maker@users.noreply.github.com>
2024-06-19 23:30:01 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,47 @@
-#define CROW_USE_BOOST true
 #define BOOST_LOG_DYN_LINK 1
 
-#include "crow.h"
 #include "utils.h"
 #include "ErrorHandlerMiddleware.h"
 
 int main() {
-    init_logging_consol_and_file();
+    init_logging_consol_and_file(boost::log::trivial::debug);
 
     crow::App<ErrorHandlerMiddleware> app;
+    BOOST_LOG_TRIVIAL(debug) << "The homepage is available at the path: http://0.0.0.0:18080";
 
     // root path { Home page }
     CROW_ROUTE(app, "/")
-    ([](){
+    ([&](const crow::request& req){
         try {
-            BOOST_LOG_TRIVIAL(info) << "Received request at /";
+            BOOST_LOG_TRIVIAL(info) << get_client_IPv4(req) << "Received request at /" << std::endl;
             return crow::response(200, "Hello world!!!");
-        }
-        catch (const std::exception& e) {
+        } catch (const std::exception& e) {
             BOOST_LOG_TRIVIAL(error) << std::string("Error: ") + e.what();
             return crow::response(500, std::string("Error: ") + e.what());
         }
     });
 
+
+    // Асинхронное чтение изображения в память
+    const std::shared_future<std::vector<char>> imageDataFutureFaviconIco = std::async(std::launch::async, loadImage, "../assets/img/favicon.jpeg").share();
+    
+    CROW_ROUTE(app, "/favicon.ico")    // Маршрут для обработки запроса изображения
+    ([&imageDataFutureFaviconIco]() {
+        try {
+            // Ожидание завершения асинхронной операции чтения изображения
+            const std::vector<char>& imageData = imageDataFutureFaviconIco.get();
+            auto res = crow::response(std::string(imageData.begin(), imageData.end()));
+            res.set_header("Content-Type", "image/jpeg");
+            return res;
+        } catch (const std::exception& e) {
+            BOOST_LOG_TRIVIAL(error) << std::string("Error: ") + e.what();
+            return crow::response(404, "Image not found");
+        }
+    });
+    
+
+
+    app.loglevel(crow::LogLevel::WARNING);
     app.port(18080).multithreaded().run();
     return 0;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,5 @@
 #include "utils.h"
 
-
 // Функция для чтения содержимого текстового файла в std::string
 std::string readFile(const std::string& filePath) {
     std::ifstream file(filePath, std::ios::in | std::ios::binary | std::ios::ate);
@@ -39,27 +38,39 @@ std::vector<char> loadImage(const std::string& filePath) {
 
 
 // Инициализация логирования
-void init_logging_consol_and_file() {
-    // Логирование в файл с ротацией по размеру и времени
-    boost::log::add_file_log(
+void init_logging_consol_and_file(const short& type_log) {
+    // Логирование в файл с ротацией по размеру (10 MB) и времени (в полноч)
+    boost::log::add_file_log (
         boost::log::keywords::file_name = "../logs/sample_%N.log",
         boost::log::keywords::rotation_size = 10 * 1024 * 1024, // 10 MB
-        boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0), // ежедневно в полночь
-        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%", // в таком виде: [2024-06-16 19:17:06.121083] [0x00007faee9e006c0] [info] Received request at /
+        boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
+        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%]  %Message%", // в таком виде: [2024-06-16 19:17:06.121083] [0x00007faee9e006c0] [info]: Received request at /
         boost::log::keywords::auto_flush = true
     );
 
     // Логирование в консоль
-    boost::log::add_console_log(
+    boost::log::add_console_log (
         std::cout, 
-        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%" // в таком виде: [2024-06-16 19:17:06.121083] [0x00007faee9e006c0] [info] Received request at /
+        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%]  %Message%" // в таком виде: [2024-06-16 19:17:06.121083] [0x00007faee9e006c0] [info]: Received request at /
     );  
 
-    // Установка уровня логирования (опционально)
-    boost::log::core::get()->set_filter(
-        boost::log::trivial::severity >= boost::log::trivial::debug
+    // Установка уровня логирования
+    boost::log::core::get()->set_filter (
+        boost::log::trivial::severity >= type_log
     );
 
     // Добавление общих атрибутов
     boost::log::add_common_attributes();
+}
+
+
+std::string get_client_IPv4(const crow::request& req) {
+    // Проверка заголовка X-Forwarded-For
+    auto x_forwarded_for = req.get_header_value("X-Forwarded-For");
+    if (!x_forwarded_for.empty()) {
+        return x_forwarded_for + std::string(": ");
+    }
+
+    // Если заголовок X-Forwarded-For отсутствует, используем адрес из соединения
+    return req.remote_ip_address + std::string(": ");
 }
